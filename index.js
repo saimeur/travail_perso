705966418ebc43cbdc638f807fcab17776d158b9
samy <samy.aimeur@telecom-paris.fr>
2023-03-25 02:15:18 +0100

    ajout de mes fichiers

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hello Word');
+
+let x = 10;
+let prenom = 'Samy';
+let tab = [];
+
+for (i=0; i<10;i++){
+    tab[i]=i;
+}
+console.log(tab);
+
+function test(x){
+    return x*x;
+}
+
+value1 = test(3);
+value2 = test(true);
+
+const pi = 3.14;
+let doc = {
+    taille : null,
+    force: undefined,
+}
